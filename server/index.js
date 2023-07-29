738986376e93ca887dab65b17e70b817995cf94d
Tushar <tusharbharane2001@gmail.com>
2023-07-30 00:52:31 +0530

    resolved cors error

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, 'dist')))
 app.use(express.json());
 app.use(cors({
-    origin: 'https://64c560b99e67f315397adbc4--resonant-creponne-e01484.netlify.app',
+    origin: 'https://main--resonant-creponne-e01484.netlify.app',
     credentials: true,
 }));
 app.use(cookieSession(
