d55112e4c82b593eff8175515543f5a4aff9f1a3
Tushar <tusharbharane2001@gmail.com>
2023-07-30 11:40:29 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -19,13 +19,12 @@ dotenv.config();
 const app = express();
 app.use(express.static(path.resolve(__dirname, 'dist')))
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://main--resonant-creponne-e01484.netlify.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+app.use(
+    cors({
+        origin: "https://main--resonant-creponne-e01484.netlify.app",
+        credentials: true,
+    })
+);
 app.use(cookieSession(
     {
         name: "session",
