24ce8f40dcfec4c43858e44e2b7de1502c328aad
Tushar <tusharbharane2001@gmail.com>
2023-08-10 13:44:23 +0530

    cleaning up and updating the code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,7 +8,6 @@ router.route('/').post(registerUser);
 router.route('/login').post(login);
 router.route('/social-login').post(socialLogin);
 
-const SUCCESS_URL = "https://main--resonant-creponne-e01484.netlify.app/social-login";
 const FAILURE_URL = "https://main--resonant-creponne-e01484.netlify.app";
 
 router.get('/linkedin',
@@ -18,10 +17,12 @@ router.get('/linkedin',
 );
 
 router.get('/linkedin/callback',
-    passport.authenticate('linkedin', {
-        successRedirect: SUCCESS_URL,
-        failureRedirect: FAILURE_URL
-    }),
+    passport.authenticate('linkedin'),
+    (req, res) => {
+        const user = req.user;
+        const serializedUser = encodeURIComponent(JSON.stringify(user));
+        res.redirect(`https://main--resonant-creponne-e01484.netlify.app/social-login/${serializedUser}`);
+    }
 );
 router.get('/twitter',
     passport.authenticate('twitter', {
@@ -38,14 +39,6 @@ router.get('/twitter/callback',
         res.redirect(`https://main--resonant-creponne-e01484.netlify.app/social-login/${serializedUser}`);
     }
 );
-
-router.get('/login/success', (req, res) => {
-    if (req.user) {
-        res.status(200).json({ user: req.user });
-    } else {
-        res.status(401).json({ error: 'Unauthorized' });
-    }
-});
 
 router.get('/logout', (req, res) => {
     req.logout();
